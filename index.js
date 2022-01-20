f9b06c9689c25dd177bc271913a74c490f482a61
Sergey Khorunjiy <sergo_235_s@mail.ru>
2022-01-20 20:26:50 +0300

    https://github.com/XSergo/https---github.com-XSergo-To-do-list.git

--- index.js
+++ index.js
@@ -1,77 +1,25 @@
-const addTaskBtn = document.getElementById('add-task-btn');
-const deskTaskinput = document.getElementById('description-task');
-const todosWrapper = document.querySelector('.todos-wrapper');
+const input = document.getElementById('input');
 
-let tasks;
-!localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'));   // создание локальной истории
+document.querySelector('#push').onclick = function(){
 
-let todoItemElems = [];
+if(input.value === 0) return;
 
-function Task(description) {
-    this.description = description;
-    this.completed = false;
+document.querySelector('#tasks').innerHTML += `
+  <div class="task">
+    <span id="taskname">
+        ${document.querySelector('#newtask input').value}
+    </span>
+    <button class="delete">
+    <i class="far fa-trash-alt"></i>
+    </button>
+  </div>
+`;
+input.value = '';
+
+const current_tasks = document.querySelectorAll(".delete");
+for(let i = 0; i < current_tasks.length; i++){
+  current_tasks[i].onclick = function(){
+    this.parentNode.remove();
+  }
 }
-
-const createTemlate = (task, index) => {  // динамическое добавление tasks, очищение строки после добавление task, вызов index по нажатию на кнопки
-    return `
-        <div class="todo-item ${task.completed ? 'checked' : ''}">
-            <div class="description">${task.description}</div>
-            <div class="buttons">
-                <input onclick="comleteTask(${index})" class="btn-comlete" type="checkbox" ${task.completed ? 'checked' : ''}>
-                <button onclick="deleteTask(${index})" class="btn-delete">Delete</button>
-            </div>
-        </div>
-    `
-}
-
-const filterTasks = () => {  // фильтрует tasks по нажатию на кнопку completed
-    const activeTasks = tasks.length && tasks.filter(item => item.completed == false);  // невыполненные задачи
-    const comletedTasks = tasks.length && tasks.filter(item => item.completed == true); // выполненные задачи
-    tasks = [...activeTasks, ...comletedTasks];
-}
-
-const fillHtmlList = () => {  // преобразованный tasks и перебираем tasks
-    todosWrapper.innerHTML = "";
-    if(tasks.length > 0) {
-        filterTasks();
-        tasks.forEach((item, index) => {
-            todosWrapper.innerHTML += createTemlate(item, index);
-        });
-        todoItemElems = document.querySelectorAll('.todo-item');
-    }
-}
-
-fillHtmlList();
-
-
-const updatelocal = () => {
-    localStorage.setItem('tasks', JSON.stringify(tasks));  // обновление локальной истории tasks
-}
-
-const comleteTask = index => {  // добавление к массиву строку 'cheked' через кнопку completed 
-    //console.log(index); проверка работы кнопки completed
-    tasks[index].completed = !tasks[index].completed;
-    if (tasks[index].completed) {
-        todoItemElems[index].classList.add('checked');
-    }else {
-        todoItemElems[index].classList.remove('checked');
-    }
-    updatelocal();
-    fillHtmlList();
-}
-
-addTaskBtn.addEventListener('click', () => {  // добавление task через слушателя 
-    tasks.push(new Task(deskTaskinput.value));
-    updatelocal();
-    fillHtmlList();
-    deskTaskinput.value = '';
-})
-
-const deleteTask = index => {   // удаление task
-    todoItemElems[index].classList.add('delition');
-    setTimeout(() => {
-        tasks.splice(index, 1);
-        updatelocal();
-        fillHtmlList();      
-    },500)
 }
